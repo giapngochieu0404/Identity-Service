a0adc8d65a8c27167d04778475f6338ca2c30b14
hieugn <hieugn@gktk.co>
2024-06-07 11:53:57 +0700

    build: add whitelist urls

--- src/main/java/com/hieuubuntu/identityservice/configuration/SecurityConfig.java
+++ src/main/java/com/hieuubuntu/identityservice/configuration/SecurityConfig.java
@@ -2,19 +2,26 @@ package com.hieuubuntu.identityservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final String[] publicEndpoints = {
+    protected static final String[] WHITELIST_URLS = new String [] {
         "/auth/login", "/auth/introspect", "/auth/logout", "/auth/introspect", "/auth/refresh-token"
     };
 
@@ -24,25 +31,45 @@ public class SecurityConfig {
         this.jwtDecoderImpl = jwtDecoderImpl;
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, publicEndpoints)
-                .permitAll()
-                .anyRequest()
-                .authenticated());
+    CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
-        // Đăng kí 1 provider manager support cho Authentication provider
-        httpSecurity.oauth2ResourceServer(
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Bean
+    public SecurityFilterChain httpPublic(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors((cors) -> cors.configurationSource(apiConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher(WHITELIST_URLS)
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                .sessionManagement(
+                        configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public SecurityFilterChain httpSecurity(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors((cors) -> cors.configurationSource(apiConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .sessionManagement(
+                        configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(
                 oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(
                                 jwtDecoderImpl)) // Cung cấp cho authentication provider 1 decoder để decode token
                         .authenticationEntryPoint(
                                 new JwtAuthenticationEntryPoint()) // Bắt lỗi 401: Do lỗi 401 xảy ra ở tầng filter, chưa
                 // vào đến tầng application nên phải bắt ở đây.
-                );
-
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
-
-        return httpSecurity.build();
+                )
+                .build();
     }
 
     @Bean
